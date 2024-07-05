646d19a2817008b8f7e65077cba202db290c58ef
TayfurSafakGencay <safak.gencay@gmail.com>
2024-07-05 22:55:19 +0300

    Level Data

---
+++ Assets/Scripts/Level/LevelData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level
+{
+  [CreateAssetMenu(fileName = "Level Data", menuName = "Create Level Data", order = 0)]
+  public class LevelData : ScriptableObject
+  {
+    [SerializeField]
+    private List<LevelVo> LevelList;
+
+    public List<LevelVo> GetLevelList()
+    {
+      return LevelList;
+    }
+  }
+
+  [Serializable]
+  public struct LevelVo
+  {
+    public List<int> NecessaryItemCount;
+  }
+}
